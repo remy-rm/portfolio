22354544af6d4f3332cad9eb0661fa9f0c3164c3
remy-rm <rremy.marquis62121@gmail.com>
2024-02-22 14:38:29 +0100

    create nav bar

---
+++ src/component/NavBar/index.tsx
@@ -0,0 +1,68 @@
+import { Home, Contact, CandlestickChart, BookMarked } from 'lucide-react'
+import './style.scss'
+import { useEffect, useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+
+function Nav() {
+  const [isNavVisible, setIsNavVisible] = useState(true)
+  const [prevScrollPos, setPrevScrollPos] = useState(window.scrollY)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY
+      const isScrollingUp = prevScrollPos > currentScrollPos
+
+      setIsNavVisible(
+        currentScrollPos < 110 || (isScrollingUp && currentScrollPos > 110),
+      )
+      setPrevScrollPos(currentScrollPos)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [prevScrollPos])
+
+  const navLinks = [
+    { to: '/', label: 'Accueil', icon: <Home size={32} strokeWidth={0.75} /> },
+    {
+      to: '/competence/',
+      label: 'Compétence',
+      icon: <CandlestickChart size={32} strokeWidth={0.75} />,
+    },
+    {
+      to: '/portfolio/',
+      label: 'Portfolio',
+      icon: <BookMarked size={32} strokeWidth={0.75} />,
+    },
+    {
+      to: '/contact/',
+      label: 'Contact',
+      icon: <Contact size={32} strokeWidth={0.75} />,
+    },
+  ]
+  const location = useLocation()
+  const currentPath = location.pathname
+
+  return (
+    <nav className={isNavVisible ? 'visible' : 'hidden'}>
+      <ul className={'nav'}>
+        {navLinks.map(({ to, label, icon }, index) => (
+          <li key={index} className={'nav__btn'}>
+            <Link
+              to={to}
+              className={`nav__btn--link ${currentPath === to ? 'nav__btn--link--active' : ''}`}
+            >
+              <div className={'nav__btn--icon'}>{icon}</div>
+              <p>{label}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav
